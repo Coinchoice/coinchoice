c895ccf79bf15cc411662259b652b453e32b6487
André Teves <teves@webeleven.com.br>
2023-03-02 12:58:53 -0700

    fixes test transaction

--- packages/backend/src/utils/getToken.ts
+++ packages/backend/src/utils/getToken.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'ethers';
 import { JsonRpcSigner } from '@ethersproject/providers';
-import ERC20 from './erc20.json';
+import * as ERC20 from './erc20.json';
 import { ERC20MockWithPermit } from '../types/ERC20MockWithPermit';
 
 const TOKEN_META = {
@@ -9,14 +9,18 @@ const TOKEN_META = {
 	WBTC: { decimals: 8, symbol: 'WBTC', name: 'Wrapped Bitcoin' },
 };
 
-const TOKEN_DICT: { [id: string]: { [chainId: number]: string } } = {
+export const TOKEN_DICT: { [id: string]: { [chainId: number]: string } } = {
 	USDC: {
 		1: '0xe9DcE89B076BA6107Bb64EF30678efec11939234',
-		5: '0xe9DcE89B076BA6107Bb64EF30678efec11939234',
+		5: '0x65aFADD39029741B3b8f0756952C74678c9cEC93',
 		80001: '0xe9DcE89B076BA6107Bb64EF30678efec11939234',
 		137: '0xe9DcE89B076BA6107Bb64EF30678efec11939234',
 	},
 	DAI: {},
+	WETH: {
+		5: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+		80001: '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889',
+	},
 };
 
 export const getToken = (
