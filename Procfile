9cfafb5293b68bd74a342d0c5a6fa6b89144ecc7
André Teves <teves@webeleven.com.br>
2023-03-03 15:31:30 -0700

    frpc test deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: cd packages/backend && npm run deploy
+frpc: cd packages/frpc/gateway && npm install && npm run run configs/quickstart_config.json
---
+++ packages/frpc/gateway/src/config.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+export const configHelp = "Config structure: { port, relay, serviceId, providers, mode, counterServiceId?, counterPeerId?}\n" +
+    "Where 'mode' can be: 'random' (default), 'round-robin' or 'quorum',\n" +
+    "'counterServiceId' and 'counterPeerId' will use local service if undefined.\n"
+    "'quorumServiceId' and 'quorumPeerId' will use local service if undefined.\n"
+
+export function readConfig(path) {
+    const rawdata = fs.readFileSync(path);
+    const config = JSON.parse(rawdata);
+
+    let errors = []
+    if (!config.port) {
+        errors.push("Specify port ('port') in config")
+    }
+    if (!config.relay) {
+        errors.push("Specify Fluence peer address ('relay') in config")
+    }
+
+    return {
+        config, errors,
+        help: configHelp
+    }
+}
