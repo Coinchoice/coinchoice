93db7cdcec37e1c37eccb3d8632ada73c3b37d55
André Teves <teves@webeleven.com.br>
2023-03-03 12:56:35 -0700

    schema definition

--- packages/browser-extension/src/composites/__generated__/definition.js
+++ packages/browser-extension/src/composites/__generated__/definition.js
@@ -1,21 +1,2 @@
 // This is an auto-generated file, do not edit manually
-export const definition = {
-	models: {
-		Wallets: {
-			id: 'kjzl6hvfrbw6c9ggfud46mmd8vs5qhpujzddvw81raf7qq69vgom67h6z1vdzxk',
-			accountRelation: { type: 'list' },
-		},
-	},
-	objects: {
-		Wallets: {
-			token: { type: 'string', required: true },
-			amount: { type: 'string', required: false },
-			edited: { type: 'datetime', required: false },
-			address: { type: 'string', required: true },
-			created: { type: 'datetime', required: true },
-			network: { type: 'integer', required: true },
-		},
-	},
-	enums: {},
-	accountData: { walletsList: { type: 'connection', name: 'Wallets' } },
-};
+export const definition = {"models":{"Wallets":{"id":"kjzl6hvfrbw6cbcme6h3u9qd9lkl4m7qd4lkn3olh7b16m03a6xzuzd53tzzva6","accountRelation":{"type":"list"}},"BasicProfile":{"id":"kjzl6hvfrbw6c57p6t99krxnhqbxb24suuvxfhq1hnqujfecq0fap356ai7sjk9","accountRelation":{"type":"single"}}},"objects":{"Wallets":{"token":{"type":"string","required":true},"amount":{"type":"string","required":false},"edited":{"type":"datetime","required":false},"address":{"type":"string","required":true},"created":{"type":"datetime","required":true},"network":{"type":"integer","required":true},"ownerId":{"type":"streamid","required":true},"owner":{"type":"view","viewType":"relation","relation":{"source":"document","model":"kjzl6hvfrbw6c57p6t99krxnhqbxb24suuvxfhq1hnqujfecq0fap356ai7sjk9","property":"ownerId"}}},"BasicProfile":{"name":{"type":"string","required":true},"emoji":{"type":"string","required":false},"gender":{"type":"string","required":false},"username":{"type":"string","required":true},"description":{"type":"string","required":false}}},"enums":{},"accountData":{"walletsList":{"type":"connection","name":"Wallets"},"basicProfile":{"type":"node","name":"BasicProfile"}}}
