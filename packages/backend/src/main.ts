226b2b3a88475026d585a75b9351bb712f140e72
André Teves <teves@webeleven.com.br>
2023-03-04 09:24:05 -0700

    enableCors

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: '*',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		preflightContinue: false,
+		optionsSuccessStatus: 204,
+	});
+	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
