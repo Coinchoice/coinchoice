77167bdcbcc008923da8c7db4d6ff6d234e93ff7
Ryan Soury <ryan@webdoodle.com.au>
2023-03-04 12:04:01 -0700

    remove temp package from head

--- packages/temp/webpack/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common.js');
-
-module.exports = (envVar) => {
-    const {env} = envVar;
-    const envConfig = require(`./webpack.${env}.js`);
-    return merge(commonConfig, envConfig);
-}
