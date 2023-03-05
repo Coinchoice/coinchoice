d54a4349514a84b79d74cc74525a2ac9948b9c9f
Ryan Soury <ryan@webdoodle.com.au>
2023-03-04 20:58:55 -0700

    update snap site styling

--- packages/meta-mask/packages/site/src/components/Header.tsx
+++ packages/meta-mask/packages/site/src/components/Header.tsx
@@ -8,70 +8,70 @@ import { SnapLogo } from './SnapLogo';
 import { Toggle } from './Toggle';
 
 const HeaderWrapper = styled.header`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 2.4rem;
-  border-bottom: 1px solid ${(props) => props.theme.colors.border.default};
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	padding: 2.4rem;
+	border-bottom: 4px solid rgb(12, 192, 223);
 `;
 
 const Title = styled.p`
-  font-size: ${(props) => props.theme.fontSizes.title};
-  font-weight: bold;
-  margin: 0;
-  margin-left: 1.2rem;
-  ${({ theme }) => theme.mediaQueries.small} {
-    display: none;
-  }
+	font-size: ${(props) => props.theme.fontSizes.title};
+	font-weight: bold;
+	margin: 0;
+	margin-left: 1.2rem;
+	${({ theme }) => theme.mediaQueries.small} {
+		display: none;
+	}
 `;
 
 const LogoWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 `;
 
 const RightContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 `;
 
 export const Header = ({
-  handleToggleClick,
+	handleToggleClick,
 }: {
-  handleToggleClick(): void;
+	handleToggleClick(): void;
 }) => {
-  const theme = useTheme();
-  const [state, dispatch] = useContext(MetaMaskContext);
+	const theme = useTheme();
+	const [state, dispatch] = useContext(MetaMaskContext);
 
-  const handleConnectClick = async () => {
-    try {
-      await connectSnap();
-      const installedSnap = await getSnap();
+	const handleConnectClick = async () => {
+		try {
+			await connectSnap();
+			const installedSnap = await getSnap();
 
-      dispatch({
-        type: MetamaskActions.SetInstalled,
-        payload: installedSnap,
-      });
-    } catch (e) {
-      console.error(e);
-      dispatch({ type: MetamaskActions.SetError, payload: e });
-    }
-  };
-  return (
-    <HeaderWrapper>
-      <LogoWrapper>
-        <CCLogo/>
-      </LogoWrapper>
-      <RightContainer>
-        <Toggle
-          onToggle={handleToggleClick}
-          defaultChecked={getThemePreference()}
-        />
-        <HeaderButtons state={state} onConnectClick={handleConnectClick} />
-      </RightContainer>
-    </HeaderWrapper>
-  );
+			dispatch({
+				type: MetamaskActions.SetInstalled,
+				payload: installedSnap,
+			});
+		} catch (e) {
+			console.error(e);
+			dispatch({ type: MetamaskActions.SetError, payload: e });
+		}
+	};
+	return (
+		<HeaderWrapper>
+			<LogoWrapper>
+				<CCLogo />
+			</LogoWrapper>
+			<RightContainer>
+				<Toggle
+					onToggle={handleToggleClick}
+					defaultChecked={getThemePreference()}
+				/>
+				<HeaderButtons state={state} onConnectClick={handleConnectClick} />
+			</RightContainer>
+		</HeaderWrapper>
+	);
 };
--- packages/meta-mask/packages/site/src/pages/index.tsx
+++ packages/meta-mask/packages/site/src/pages/index.tsx
@@ -2,199 +2,202 @@ import { useContext } from 'react';
 import styled from 'styled-components';
 import { MetamaskActions, MetaMaskContext } from '../hooks';
 import {
-  connectSnap,
-  getSnap,
-  sendHello,
-  shouldDisplayReconnectButton,
+	connectSnap,
+	getSnap,
+	sendHello,
+	shouldDisplayReconnectButton,
 } from '../utils';
 import {
-  ConnectButton,
-  InstallFlaskButton,
-  ReconnectButton,
-  SendHelloButton,
-  Card,
+	ConnectButton,
+	InstallFlaskButton,
+	ReconnectButton,
+	SendHelloButton,
+	Card,
 } from '../components';
 import { CCLogoFlex } from '../components/CCLogo';
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  flex: 1;
-  margin-top: 7.6rem;
-  margin-bottom: 7.6rem;
-  ${({ theme }) => theme.mediaQueries.small} {
-    padding-left: 2.4rem;
-    padding-right: 2.4rem;
-    margin-top: 2rem;
-    margin-bottom: 2rem;
-    width: auto;
-  }
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	flex: 1;
+	margin-top: 7.6rem;
+	margin-bottom: 7.6rem;
+	${({ theme }) => theme.mediaQueries.small} {
+		padding-left: 2.4rem;
+		padding-right: 2.4rem;
+		margin-top: 2rem;
+		margin-bottom: 2rem;
+		width: auto;
+	}
 `;
 
 const Heading = styled.h1`
-  margin-top: 0;
-  margin-bottom: 2.4rem;
-  text-align: center;
+	margin-top: 0;
+	margin-bottom: 2.4rem;
+	text-align: center;
 `;
 
 const Span = styled.span`
-  color: ${(props) => props.theme.colors.primary.default};
+	color: ${(props) => props.theme.colors.primary.default};
 `;
 
 const Subtitle = styled.p`
-  font-size: ${({ theme }) => theme.fontSizes.large};
-  font-weight: 500;
-  margin-top: 0;
-  margin-bottom: 0;
-  ${({ theme }) => theme.mediaQueries.small} {
-    font-size: ${({ theme }) => theme.fontSizes.text};
-  }
+	font-size: ${({ theme }) => theme.fontSizes.large};
+	font-weight: 500;
+	margin-top: 0;
+	margin-bottom: 0;
+	${({ theme }) => theme.mediaQueries.small} {
+		font-size: ${({ theme }) => theme.fontSizes.text};
+	}
 `;
 
 const CardContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  max-width: 64.8rem;
-  width: 100%;
-  height: 100%;
-  margin-top: 1.5rem;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: space-between;
+	max-width: 64.8rem;
+	width: 100%;
+	height: 100%;
+	margin-top: 1.5rem;
 `;
 
 const Notice = styled.div`
-  background-color: ${({ theme }) => theme.colors.background.alternative};
-  border: 1px solid ${({ theme }) => theme.colors.border.default};
-  color: ${({ theme }) => theme.colors.text.alternative};
-  border-radius: ${({ theme }) => theme.radii.default};
-  padding: 2.4rem;
-  margin-top: 2.4rem;
-  max-width: 60rem;
-  width: 100%;
+	background-color: ${({ theme }) => theme.colors.background.alternative};
+	border: 1px solid ${({ theme }) => theme.colors.border.default};
+	color: ${({ theme }) => theme.colors.text.alternative};
+	border-radius: ${({ theme }) => theme.radii.default};
+	padding: 2.4rem;
+	margin-top: 2.4rem;
+	max-width: 60rem;
+	width: 100%;
 
-  & > * {
-    margin: 0;
-  }
-  ${({ theme }) => theme.mediaQueries.small} {
-    margin-top: 1.2rem;
-    padding: 1.6rem;
-  }
+	& > * {
+		margin: 0;
+	}
+	${({ theme }) => theme.mediaQueries.small} {
+		margin-top: 1.2rem;
+		padding: 1.6rem;
+	}
 `;
 
 const ErrorMessage = styled.div`
-  background-color: ${({ theme }) => theme.colors.error.muted};
-  border: 1px solid ${({ theme }) => theme.colors.error.default};
-  color: ${({ theme }) => theme.colors.error.alternative};
-  border-radius: ${({ theme }) => theme.radii.default};
-  padding: 2.4rem;
-  margin-bottom: 2.4rem;
-  margin-top: 2.4rem;
-  max-width: 60rem;
-  width: 100%;
-  ${({ theme }) => theme.mediaQueries.small} {
-    padding: 1.6rem;
-    margin-bottom: 1.2rem;
-    margin-top: 1.2rem;
-    max-width: 100%;
-  }
+	background-color: ${({ theme }) => theme.colors.error.muted};
+	border: 1px solid ${({ theme }) => theme.colors.error.default};
+	color: ${({ theme }) => theme.colors.error.alternative};
+	border-radius: ${({ theme }) => theme.radii.default};
+	padding: 2.4rem;
+	margin-bottom: 2.4rem;
+	margin-top: 2.4rem;
+	max-width: 60rem;
+	width: 100%;
+	${({ theme }) => theme.mediaQueries.small} {
+		padding: 1.6rem;
+		margin-bottom: 1.2rem;
+		margin-top: 1.2rem;
+		max-width: 100%;
+	}
 `;
 
 const Index = () => {
-  const [state, dispatch] = useContext(MetaMaskContext);
+	const [state, dispatch] = useContext(MetaMaskContext);
 
-  const handleConnectClick = async () => {
-    try {
-      await connectSnap();
-      const installedSnap = await getSnap();
+	const handleConnectClick = async () => {
+		try {
+			await connectSnap();
+			const installedSnap = await getSnap();
 
-      dispatch({
-        type: MetamaskActions.SetInstalled,
-        payload: installedSnap,
-      });
-    } catch (e) {
-      console.error(e);
-      dispatch({ type: MetamaskActions.SetError, payload: e });
-    }
-  };
+			dispatch({
+				type: MetamaskActions.SetInstalled,
+				payload: installedSnap,
+			});
+		} catch (e) {
+			console.error(e);
+			dispatch({ type: MetamaskActions.SetError, payload: e });
+		}
+	};
 
-  const handleSendHelloClick = async () => {
-    try {
-      await sendHello();
-    } catch (e) {
-      console.error(e);
-      dispatch({ type: MetamaskActions.SetError, payload: e });
-    }
-  };
+	const handleSendHelloClick = async () => {
+		try {
+			await sendHello();
+		} catch (e) {
+			console.error(e);
+			dispatch({ type: MetamaskActions.SetError, payload: e });
+		}
+	};
 
-  return (
-    <Container>
-      <CCLogoFlex size={'200px'} />
-      <Heading>
-        Welcome to <Span>Coinchoice</Span>
-      </Heading>
-      <Subtitle>
-        Install our Snap to never worry about having enough ETH for gas ever again.
-      </Subtitle>
-      <CardContainer>
-        {state.error && (
-          <ErrorMessage>
-            <b>An error happened:</b> {state.error.message}
-          </ErrorMessage>
-        )}
-        {!state.isFlask && (
-          <Card
-            content={{
-              title: 'Install',
-              description:
-                'Snaps is pre-release software only available in MetaMask Flask, a canary distribution for developers with access to upcoming features.',
-              button: <InstallFlaskButton />,
-            }}
-            fullWidth
-          />
-        )}
-        {!state.installedSnap ? (
-          <Card
-          fullWidth
-            content={{
-              title: 'Connect',
-              description:
-                'Get started by connecting to and installing our snap.',
-              button: (
-                <ConnectButton
-                  onClick={handleConnectClick}
-                  disabled={!state.isFlask}
-                />
-              ),
-            }}
-            disabled={!state.isFlask}
-          />
-        ) : <Card
-        fullWidth
-          content={{
-            title: 'Installed!',
-            description:
-              'You now can easily receive ETH when triggering a transaction and clicking on the "Need ETH?" tab.',
-          }}
-          disabled={!state.isFlask}
-        />}
-        {shouldDisplayReconnectButton(state.installedSnap) && (
-          <Card
-            content={{
-              title: 'Reconnect',
-              description:
-                'While connected to a local running snap this button will always be displayed in order to update the snap if a change is made.',
-              button: (
-                <ReconnectButton
-                  onClick={handleConnectClick}
-                  disabled={!state.installedSnap}
-                />
-              ),
-            }}
-            disabled={!state.installedSnap}
-          />
-        )}
-        {/* <Card
+	return (
+		<Container>
+			<CCLogoFlex size={'200px'} />
+			<Heading>
+				Welcome to <Span color="rgb(12, 192, 223)">CoinChoice</Span>
+			</Heading>
+			<Subtitle>
+				Install our Snap to never worry about having enough ETH for gas ever
+				again.
+			</Subtitle>
+			<CardContainer>
+				{state.error && (
+					<ErrorMessage>
+						<b>An error happened:</b> {state.error.message}
+					</ErrorMessage>
+				)}
+				{!state.isFlask && (
+					<Card
+						content={{
+							title: 'Install',
+							description:
+								'Snaps is pre-release software only available in MetaMask Flask, a canary distribution for developers with access to upcoming features.',
+							button: <InstallFlaskButton />,
+						}}
+						fullWidth
+					/>
+				)}
+				{!state.installedSnap ? (
+					<Card
+						fullWidth
+						content={{
+							title: 'Connect',
+							description:
+								'Get started by connecting to and installing our snap.',
+							button: (
+								<ConnectButton
+									onClick={handleConnectClick}
+									disabled={!state.isFlask}
+								/>
+							),
+						}}
+						disabled={!state.isFlask}
+					/>
+				) : (
+					<Card
+						fullWidth
+						content={{
+							title: 'Installed!',
+							description:
+								'You now can easily receive ETH when triggering a transaction and clicking on the "Need ETH?" tab.',
+						}}
+						disabled={!state.isFlask}
+					/>
+				)}
+				{shouldDisplayReconnectButton(state.installedSnap) && (
+					<Card
+						content={{
+							title: 'Reconnect',
+							description:
+								'While connected to a local running snap this button will always be displayed in order to update the snap if a change is made.',
+							button: (
+								<ReconnectButton
+									onClick={handleConnectClick}
+									disabled={!state.installedSnap}
+								/>
+							),
+						}}
+						disabled={!state.installedSnap}
+					/>
+				)}
+				{/* <Card
           content={{
             title: 'Send Hello message',
             description:
@@ -213,17 +216,17 @@ const Index = () => {
             !shouldDisplayReconnectButton(state.installedSnap)
           }
         /> */}
-        <Notice>
-          <p>
-            Please note that the <b>snap.manifest.json</b> and{' '}
-            <b>package.json</b> must be located in the server root directory and
-            the bundle must be hosted at the location specified by the location
-            field.
-          </p>
-        </Notice>
-      </CardContainer>
-    </Container>
-  );
+				<Notice>
+					<p>
+						Please note that the <b>snap.manifest.json</b> and{' '}
+						<b>package.json</b> must be located in the server root directory and
+						the bundle must be hosted at the location specified by the location
+						field.
+					</p>
+				</Notice>
+			</CardContainer>
+		</Container>
+	);
 };
 
 export default Index;
